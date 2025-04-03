dd46ef5779d429083ab882a0e2644de8d914792b
SiyaTM <60353073+SiyaTM@users.noreply.github.com>
2025-04-03 14:28:32 +0200

    Add files via upload

---
+++ threads-clone/app/(tabs)/index.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import { 
+  Platform, 
+  RefreshControl, 
+  SafeAreaView, 
+  ScrollView, 
+  StyleSheet,
+} from 'react-native';
+
+import { Text, View } from '@/components/Themed';
+import Lottie from "lottie-react-native";
+import { ThreadsContext } from '../../context/thread-context';
+import ThreadsItem from '@/components/ThreadsItem';
+
+
+export default function TabOneScreen() {
+  const animationRef = React.useRef<Lottie>(null);
+  const threads = React.useContext(ThreadsContext)
+
+  return (
+    <SafeAreaView>
+      <ScrollView
+        contentContainerStyle={{
+          paddingHorizontal: 10,
+          paddingTop: Platform.select({ android: 30 }),
+        }}
+        refreshControl={
+          <RefreshControl
+            refreshing={false}
+            onRefresh={() => {
+              animationRef.current?.play();
+            }}
+            tintColor={"transparent"}
+          />
+        }
+      >
+        <Lottie 
+          ref={animationRef}
+          source={require("../../lottie-animations/threads.json")}
+          loop={false}
+          autoPlay
+          style={{ 
+            width: 90, 
+            height: 90,
+            alignSelf: "center",
+           }}
+          //  onAnimationFinish={() => {
+          //   alert("finished")
+          //  }}
+        />
+        {threads.map((thread) => (
+          <ThreadsItem key={thread.id} {...thread} />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+
+  );
+}
+
